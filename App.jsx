bd4605b4e0d84c3149cdee1a4c9e93b0897cf66d
Matthy.J. de Hoog <mdehoog@x-connections.com>
2026-03-19 10:57:53 +0100

    test

--- App.jsx
+++ App.jsx
@@ -1053,11 +1053,6 @@ function App() {
           </div>
         )}
 
-{scherm === "cirkel" && (
-  <div style={{background:"red",color:"white",padding:10}}>
-    cirkelId: {cirkelId} | cirkel: {cirkel?.naam || "null"} | gebruiker: {gebruiker?.naam}
-  </div>
-)}
 
         {/* ══ CIRKEL ══ */}
         {scherm === "cirkel" && cirkel && (
