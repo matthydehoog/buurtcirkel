076729487fcad98082c4735419b8bdf965248f5a
Matthy.J. de Hoog <mdehoog@x-connections.com>
2026-03-20 08:57:16 +0100

    CL

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-const APP_VERSIE = "2.8.4";
+const APP_VERSIE = "2.9.0";
 
 // ─── SUPABASE CONFIG ───────────────────────────────────────────────
 const SUPABASE_URL = "https://uztplrszzpwywhvsmoqz.supabase.co";
